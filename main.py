45f5d5ccbb3ddf0e000de85095f79a40b1e380ed
Marcin Niemczyk <m_niemczyk@outlook.com>
2023-10-01 07:45:51 +0200

    working backend connection locally

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers import survey, admin
 from sql.database import SessionLocal, engine
@@ -8,9 +10,20 @@ import os
 
 os.remove("sql_app.db")
 
-
 app = FastAPI()
 
+
+origins = [
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -19,4 +32,5 @@ app.include_router(admin.router)
 
 populate(10, 20)
 
-
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- quick_sign/app/panel/page.tsx
+++ quick_sign/app/panel/page.tsx
@@ -1,79 +1,93 @@
 'use client'
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
+axios.defaults.baseURL = 'http://127.0.0.1:8000';
+
 function Survey() {
-  const [userResponse, setUserResponse] = useState(0);
-  const [question, setQuestion] = useState('');
-  const [questionId, setQuestionId] = useState(0);
-  const [error, setError] = useState(null);
+    const [userResponse, setUserResponse] = useState(0);
+    const [question, setQuestion] = useState('');
+    const [questionId, setQuestionId] = useState(0);
+    const [error, setError] = useState(null);
+    const [questions, setQuestions] = useState<{ [key: string]: number }>({});
 
-  useEffect(() => {
-    // Fetch the question from the backend when the component mounts
-    fetchQuestion();
-  }, []);
+    useEffect(() => {
+        fetchQuestion();
+    }, []);
 
-  const fetchQuestion = async () => {
-    try {
-      setError(null); // Clear any previous error
-      const response = await axios.get('/api/getQuestion'); // Replace with your backend API endpoint
-      const { id, text } = response.data;
-      setQuestionId(id);
-      setQuestion(text);
-    } catch (error) {
-      console.error('Error fetching question:', error);
-      setError('Failed to load the question. Please try again later.');
-    }
-  };
+    const addQuestion = () => {
+        if (question) {
+            setQuestions(prev => ({
+                ...prev,
+                [question]: questionId
+            }));
+            setQuestion('');
+            setQuestionId(prevId => prevId + 1);
+        }
+    };
 
-  const handleResponse = async () => {
-    try {
-      // Send the user response to the backend
-      await axios.post('/api/submitResponse', {
-        questionId: questionId,
-        responseValue: userResponse,
-      });
+    const fetchQuestion = async () => {
+        try {
+            setError(null);
+            const response = await axios.post('/survey/', questions);
+            const {id, text} = response.data;
+            setQuestionId(id);
+            setQuestion(text);
+            addQuestion();
+        } catch (error) {
+            console.error('Error fetching question:', error);
+            setError('Failed to load the question. Please try again later.');
+        }
+    };
 
-      // Fetch the next question
-      fetchQuestion();
-    } catch (error) {
-      console.error('Error submitting response:', error);
-    }
-  };
+    const handleResponse = async () => {
+        try {
+            // Send the user response to the backend
+            await axios.post('/api/submitResponse', {
+                questionId: questionId,
+                responseValue: userResponse,
+            });
 
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-gray-300 rounded-lg shadow p-10 w-96">
-        {error ? (
-          <p className="text-red-600">{error}</p>
-        ) : (
-          <div>
-            <p className="text-xl font-semibold">{question}</p>
-            <div className="flex items-center space-x-2 mt-2">
-              <input
-                type="range"
-                min="-2"
-                max="2"
-                step="1"
-                defaultValue="0"
-                className="w-48"
-                onChange={(e) => setUserResponse(parseInt(e.target.value))}
-              />
-              <span className="text-gray-600">
+            // Fetch the next question
+            fetchQuestion();
+        } catch (error) {
+            console.error('Error submitting response:', error);
+        }
+    };
+
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <div className="bg-gray-300 rounded-lg shadow p-10 w-96">
+                {error ? (
+                    <p className="text-red-600">{error}</p>
+                ) : (
+                    <div>
+                        <p className="text-xl font-semibold">{question}</p>
+                        <div className="flex items-center space-x-2 mt-2">
+                            <input
+                                type="range"
+                                min="-2"
+                                max="2"
+                                step="1"
+                                defaultValue="0"
+                                className="w-48"
+                                onChange={(e) => setUserResponse(parseInt(e.target.value))}
+                            />
+                            <span className="text-gray-600">
                 {['Rawly Not', 'Rather Not', 'I Do Not Know', 'Rather Yes', 'Rawly Yes'][userResponse + 2]}
               </span>
+                        </div>
+                    </div>
+                )}
+                <button
+                    onClick={handleResponse}
+                    className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md"
+                >
+                    Next
+                </button>
             </div>
-          </div>
-        )}
-        <button
-          onClick={handleResponse}
-          className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md"
-        >
-          Next
-        </button>
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default Survey;
