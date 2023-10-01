74cecd7659e0df528a458c717f4fb6456b97c3af
Adam <MorfeuszowyMorfeusz@gmail.com>
2023-10-01 02:01:14 +0200

    updated schemas

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from fastapi import FastAPI
-from routers import users, certificates
 from sql.database import SessionLocal, engine
 from sql import models
 
 app = FastAPI()
 
-app.include_router(users.router)
-app.include_router(certificates.router)
 
 models.Base.metadata.create_all(bind=engine)
 
--- sql/schemas.py
+++ sql/schemas.py
@@ -1,16 +1,18 @@
 from pydantic import BaseModel
 
 
-class UserBase(BaseModel):
-    email: str
-
-
-class UserCreate(UserBase):
-    password: str
-
-
-class User(UserBase):
-    id: int
+class Question(BaseModel):
+    lastQuestions: dict[int, int]
 
     class Config:
-        orm_mode = True
+        from_attributes = True
+
+
+class Response(BaseModel):
+    questionId: int
+    questionText: str
+    class Config:
+        from_attributes = True
+
+
+
